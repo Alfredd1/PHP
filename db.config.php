e8dccfb504582901079e03f0160ad3b45701d107
Alfred Ranz Navarro <gnavarroalfred@gmail.com>
2025-11-25 15:40:01 -0500

    vendor and db.config.php

---
+++ db.config.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'app' => [
+        'host' => 'localhost',
+        'username' => 'username',
+        'password' => 'password',
+        'dbname' => 'dbname'
+    ],
+    'test' => [
+        'host' => 'localhost',
+        'username' => 'username',
+        'password' => 'password',
+        'dbname' => 'test_dbname'
+    ],
+];
