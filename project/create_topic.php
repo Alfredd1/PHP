268694425bb7c254c424cfa654e2fd9b92d39d12
keaganPC <keagan.cameron@georgebrown.ca>
2025-11-28 11:56:06 -0500

    create looking nice

--- project/create_topic.php
+++ project/create_topic.php
@@ -29,16 +29,25 @@
 <body>
 
     <main>
-        <h2>Create a topic!</h2>
-        <form action="create_topic.php" method="POST">
-            <label>
+        <div>
+            <h2 id="title">Create a topic!</h2>
+            <form action="create_topic.php" method="POST">
+
+                <label >title </label>
+                <br>
                 <input type="text" name="title" value="">
-            </label>
-            <label>
+
+                <br>
+
+                <label>description </label>
+                <br>
                 <input type="text" name="description" value="">
-            </label>
-            <button type="submit">Submit</button>
-        </form>
+
+                <br>
+
+                <button id="create" type="submit">Create Topic</button>
+            </form>
+        </div>
     </main>
     <footer>
 
@@ -46,4 +55,49 @@
 
 </body>
 </html>
+
+    <style>
+        #title{
+            color: black;
+            font-size: 60px;
+        }
+        div {
+            width: 500px;
+            height: 500px;
+            margin: 4em auto;
+            background-color: #E6E6FA;
+            border: 1px solid #ccc;
+            border-radius: 10px;
+
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+        }
+        form {
+            margin-top: 20px;
+            color: black;
+        }
+        p{
+            color: black;
+        }
+        input{
+            margin-bottom: 20px;
+            font-size: 25px;
+            border-radius: 10px;
+        }
+        label{
+            font-size: 30px;
+        }
+        #create{
+            border-radius: 12px;
+
+            padding: 20px 40px 20px 40px;
+        }
+        #create:hover{
+            background-color:#cfcffc;
+        }
+
+    </style>
 <?php /*show_source(__FILE__) */?>
