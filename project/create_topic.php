f58bfcf54c8fceb7b1d0e517519ed60f972ae593
keagan <keagancam@gmail.com>
2025-11-28 17:19:28 -0500

    added session check before starting

--- project/create_topic.php
+++ project/create_topic.php
@@ -1,7 +1,9 @@
 <?php
     include 'nav.php';
     include "functions.php";
-    session_start();
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
     if (isset($_POST['theme'])) {
         setTheme();
         exit;
