c796295710bf951e39c06421cfd57f4fa6a1859e
keaganPC <keagan.cameron@georgebrown.ca>
2025-11-28 14:52:18 -0500

    fixed index. now redirects to login.

---
+++ project/index.php
@@ -0,0 +1,26 @@
+
+<?php
+    include "functions.php";
+    session_start();
+
+    header("Location: login.php"); // instantly redirects to login ( this is for the server )
+    exit;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Redirection page</title>
+</head>
+<body>
+
+    <main>
+        <p>This is the dashboard</p>
+    </main>
+    <footer>
+
+    </footer>
+
+</body>
+</html>
+<?php /*show_source(__FILE__) */?>
