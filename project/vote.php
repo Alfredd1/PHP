3081572cdb9c53efea541e341b4c9c1ef120d34b
keaganPC <keagan.cameron@georgebrown.ca>
2025-11-28 13:45:09 -0500

    minute change

--- project/vote.php
+++ project/vote.php
@@ -32,7 +32,7 @@
 
 <body>
     <main>
-        <div id="warning" ">
+        <div id="warning" >
             You Already Voted for on this topic.
         </div>
         <h2 id="pageTitle">Topic List</h2>
