5aa7010414e6520e796b62beb9f02c5d0b6ef82f
keaganPC <keagan.cameron@georgebrown.ca>
2025-11-28 11:43:58 -0500

    topics page lookin nice

--- project/vote.php
+++ project/vote.php
@@ -32,24 +32,34 @@
 
 <body>
     <main>
-        <h2>Topic List</h2>
-        <?php foreach ($topicList as $topic): ?>
+        <h2 id="pageTitle">Topic List</h2>
+        <div id="grid-wrapper">
+            <?php foreach ($topicList as $topic): ?>
 
-        <p><?= $topic['topicID'] ?> | <?= $topic['creator'] ?> | <?= $topic['title'] ?> | <?= $topic['description'] ?> </p>
-        <form method="POST">
-            <input type="hidden" name="topicID" value="<?= $topic['topicID'] ?>">
-            <button type="submit" name="voteType" value="up">Upvote</button>
-            <button type="submit" name="voteType" value="down">Downvote</button>
-            <p><?= print_r(getVoteResults($topic['topicID']), true) ?></p>
-        </form>
+            <div class="eventBox">
+                <h2 class="topicTitle"><?= $topic['title'] ?></h2>
+                <p>Created By: <?= $topic['creator'] ?></p>
+                <p>ID: <?= $topic['topicID'] ?></p>
+                <br>
+                <p>Description: <?= $topic['description'] ?></p>
 
-        <?php endforeach; ?>
+                <form method="POST">
+                    <input type="hidden" name="topicID" value="<?= $topic['topicID'] ?>">
+                    <button type="submit" name="voteType" value="up" class="upvoteBtn" ><?= getVoteResults($topic['topicID'])["up"] ?> <br>  Upvote</button>
+                    <button type="submit" name="voteType" value="down" class="downvoteBtn" ><?= getVoteResults($topic['topicID'])["down"] ?> <br>  Downvote</button>
 
-        <?php if ($voted): ?>
-            <p>You have already voted!</p>
-        <?php endif ?>
+                </form>
+            </div>
+
+            <?php endforeach; ?>
+
+            <?php if ($voted): ?>
+                <p>You have already voted!</p>
+            <?php endif ?>
+        </div>
 
     </main>
+
     <footer>
 
     </footer>
@@ -57,5 +67,55 @@
 </body>
 
 </html>
+
+
+<style>
+    #pageTitle{
+        text-align: center;
+        font-size: 70px;
+    }
+    .eventBox{
+        background-color: #E6E6FA;
+        border: black 2px solid;
+        border-radius: 5px;
+
+        width: 308px;
+
+        padding: 10px;
+        margin-bottom: 15px;
+    }
+    p{
+        margin-bottom: 5px;
+        margin-top: 5px;
+    }
+    .topicTitle{
+        margin-bottom: 10px;
+        text-align: center;
+    }
+    button {
+        height: 60px;
+        width: 150px;
+        border-radius: 7px;
+    }
+    .downvoteBtn:hover{
+        background-color: #e3495e;
+    }
+    .upvoteBtn:hover{
+        background-color: #4dab64;
+    }
+    #grid-wrapper {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, 308px);
+        gap: 40px;
+        justify-content: center; /* Center the whole grid */
+    }
+
+</style>
+
+
+<script>
+
+
+</script>
 
 <?php /*show_source(__FILE__) */?>
